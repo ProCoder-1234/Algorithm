0498b472ba921e80efb9ea6aa3abdee531731d09
ProCoder-1234 <66902723+ProCoder-1234@users.noreply.github.com>
2020-06-18 20:13:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+
+  movingRect=createSprite(400, 200, 50, 70);
+  movingRect.shapeColor="green";
+  movingRect.debug=true;
+  
+  fixedRect=createSprite(600, 300, 50, 70);
+  fixedRect.shapeColor="green";
+  fixedRect.debug=true;
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+movingRect.x=mouseX;
+movingRect.y=mouseY;
+console.log(movingRect.x-fixedRect.x);
+
+if (movingRect.x-fixedRect.x <= fixedRect.width/2+movingRect.width/2 &&
+   fixedRect.x-movingRect.x <= fixedRect.width/2+movingRect.width/2  && 
+   movingRect.y-fixedRect.y <= fixedRect.height/2+movingRect.height/2 &&
+   fixedRect.y-movingRect.y <= fixedRect.height/2+movingRect.height/2 ){
+
+   movingRect.shapeColor="red";
+   fixedRect.shapeColor="red";
+}
+else {
+  movingRect.shapeColor="green";
+   fixedRect.shapeColor="green";
+}
+
+  drawSprites();
+}
